c67b99cc4caa0f3a283bb2cdc182b5c4345c6537
Mihai Giurgeanu <mihai.giurgeanu@gmail.com>
2015-03-22 19:08:46 +0200

    Generated Visual Studio Console App.

---
+++ fingerprints_service/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fingerprints_service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
